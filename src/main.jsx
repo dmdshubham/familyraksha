eb75ee8c4a340b3325da74f928086cacdfa6c422
dmdshubham <dmdshubham@gmail.com>
2025-10-31 17:37:28 +0530

    build fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import WOW from 'wowjs'
+import * as WOWLib from 'wowjs'
 import 'animate.css'
 import './index.css'
 import App from './App.jsx'
 
 // Initialize WOW.js after component mounts
 setTimeout(() => {
-  const wow = new WOW.WOW({
-    boxClass: 'wow',
-    animateClass: 'animated',
-    offset: 50,
-    mobile: false,
-    live: true
-  })
-  wow.init()
+  const WOW = WOWLib.WOW || WOWLib.default?.WOW || window.WOW
+  if (WOW) {
+    const wow = new WOW({
+      boxClass: 'wow',
+      animateClass: 'animated',
+      offset: 50,
+      mobile: false,
+      live: true
+    })
+    wow.init()
+  }
 }, 100)
 
 createRoot(document.getElementById('root')).render(
