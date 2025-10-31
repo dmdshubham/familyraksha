d07acd5cad168dadfb8c01cd2ac1773d6584ed96
dmdshubham <dmdshubham@gmail.com>
2025-10-31 18:18:35 +0530

    build fixed

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import * as WOWLib from 'wowjs'
 import 'animate.css'
 import './index.css'
 import App from './App.jsx'
 
-// Initialize WOW.js after component mounts
-setTimeout(() => {
-  const WOW = WOWLib.WOW || WOWLib.default?.WOW || window.WOW
-  if (WOW) {
-    const wow = new WOW({
+// Initialize WOW.js after page loads
+window.addEventListener('load', () => {
+  if (typeof window.WOW !== 'undefined') {
+    const wow = new window.WOW({
       boxClass: 'wow',
       animateClass: 'animated',
       offset: 50,
@@ -19,7 +17,7 @@ setTimeout(() => {
     })
     wow.init()
   }
-}, 100)
+})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
