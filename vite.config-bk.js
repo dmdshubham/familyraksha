f8a944c37ac281e23cbbddf98ac9da56f0118c17
Ubuntu <ubuntu@ip-172-31-36-106.ap-south-1.compute.internal>
2025-11-06 12:45:44 +0000

    deployement fixed

--- vite.config-bk.js
+++ vite.config-bk.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  preview: {
-    allowedHosts: ['health.familyraksha.com'],  // ✅ Add this line
-    port: 3004,
-    host: '0.0.0.0',
-  },
 })
